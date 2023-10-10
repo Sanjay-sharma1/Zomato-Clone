7151ba66a7404bd89b59a85b3ba97261da513eed
Sanjay-sharma1 <sanjaysharma6189@gmail.com>
2023-10-10 22:18:27 +0530

    OverReacted Zomato

---
+++ src/pages/home/index.js
@@ -0,0 +1,11 @@
+import React  from "react";
+import Header from "../../components/common/header/header";
+import TabOptions from "../../components/common/tabOptions/firstTab";
+
+const HomePage = () => {
+    return <div>
+        <Header/>
+        <TabOptions/>
+    </div>
+}
+export default HomePage;
